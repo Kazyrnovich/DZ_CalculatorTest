146570202241371448aa0a6c9733ed87757b090d
Kaz <choco74@mail.ru>
2020-02-18 15:43:46 +0300

    Первый тест с результатом Your category is Overweight

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class CalcTest {
     @Test
@@ -8,6 +11,14 @@ public class CalcTest {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.get("https://healthunify.com/bmicalculator/");
+        browser.findElement(By.name("wg")).sendKeys("78");
+        browser.findElement(By.name("ht")).sendKeys("165");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Overweight", "Категория неверная");
+        System.out.println(category);
+
+        browser.quit();
 
     }
 }
